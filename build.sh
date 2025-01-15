2227e1b93fdd235a48c5929503741cae144a81e8
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-01-14 19:53:37 -0500

    Add testing

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t cyberark/conjur:andrewjdawes-test .
--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 # assert that CONJUR_ADMIN_ACCOUNT is set
 if [ -z "$CONJUR_ADMIN_ACCOUNT" ]; then
     echo "CONJUR_ADMIN_ACCOUNT is not set. Exiting..."
@@ -20,27 +21,13 @@ SERVICE_PID=$!
 
 # Wait for the service to be ready (e.g., polling a health check or checking a port)
 echo "Waiting for the service to be ready..."
-# while ! nc -z localhost 80; do
-#     sleep 1
-# done
 conjurctl wait
 echo "Service is ready!"
 
 # Run the CLI command
 echo "Running the CLI command..."
-echo -n "${CONJUR_ADMIN_PASSWORD}" | conjurctl account create --password-from-stdin --name "${CONJUR_ADMIN_ACCOUNT}" || echo "Failed to create account"
+echo -n "${CONJUR_ADMIN_PASSWORD}" | (conjurctl account create --password-from-stdin --name "${CONJUR_ADMIN_ACCOUNT}" || echo "Failed to create account")
 
-# Stop the service
-# echo "Stopping the service..."
-# kill "$SERVICE_PID"
-
-# forground the SERVICE_PID
-# This is needed to keep the service running
-# fg "$SERVICE_PID"
-# echo "Foregrounding the service..."
-# fg
-
-# conjurctl server
 # Keep the service running
 echo "Keeping the service running..."
 wait "$SERVICE_PID"
